77a11a533f21d2ee1ccd4e0ccc28c8691fded9a3
indahali <72369865+indahali@users.noreply.github.com>
2021-10-07 10:04:02 +0700

    navbar screening

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,41 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light shadow-sm">
+    <div class="container">
+    <img src="assets/img/surveyasia.png" alt="Logo" style="width:150px;height:40px;">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+            <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        ID
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">ID</a></li>
+                        <li><a class="dropdown-item" href="#">EN</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item ms-3 me-2">
+                    <div class="vl"></div>
+                </li>
+                <li><img src="/assets/img/prof_pic.png" alt="Profile Picture" width="32"></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        David
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
