978b41158fbb385d23ca0a4d0a4dcfef5437e767
Fahlevi Usama Afghani <fahleviafghani21@gmail.com>
2021-10-20 17:15:11 +0700

    back end login admin

--- routes/web.php
+++ routes/web.php
@@ -151,20 +151,17 @@ Route::middleware(['is_admin'])->group(function () {
 
 /* admin auth routes */
 // login
-Route::view('/admin-login', 'admin.auth.login')->name('view-admin-login');
+Route::view('/admin-login', 'admin.auth.login-admin')->name('view-admin-login');
 Route::post('/admin-login', [\App\Http\Controllers\Admin\AuthController::class, 'attemptLogin'])->name('attempt-admin-login');
 
 // register
 Route::view('/admin-register', 'admin.auth.register')->name('view-admin-register');
 Route::post('/admin-register', [\App\Http\Controllers\Admin\AuthController::class, 'attemptRegister'])->name('attempt-admin-register');
 
-//View dashboard admin by fe
-Route::get('/login-admin', function () {
-    return view('admin.auth.login-admin');
-});
+
 Route::get('/admin/list-user', function () {
     return view('admin.auth.list-user');
-  })->name('admin-users');
+})->name('admin-users');
 Route::get('/admin/news', function () {
     return view('admin.auth.news');
 });
