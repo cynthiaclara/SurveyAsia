02b6877715f4e46c4fbadc04fb0d7d5d272309a9
oksa satya <oksasatyaa@gmail.com>
2021-10-20 18:39:43 +0700

    Update 2021_10_19_074135_add_surveys_foreign_to_users_table.php

---
+++ database/migrations/2021_10_19_074135_add_surveys_foreign_to_users_table.php
@@ -0,0 +1,35 @@
+nes (30 sloc) 764 Bytes
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSurveysForeignToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /* Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('survey_id');
+            $table->foreign('survey_id')->references('id')->on('surveys')->cascadeOnDelete();
+        }); */
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        /* Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('survey_id');
+        }); */
+    }
+}
