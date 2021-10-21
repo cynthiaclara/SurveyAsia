f67f158c8e25e25d4c862755d7fd5529cfe80d97
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-10-21 19:45:16 +0700

    Update Pricing Page

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,36 @@
 function RolesButton() {
     const researcher = document.getElementById("researcher");
     if (researcher.checked) {
-        window.location.href = "/researcher/dashboard";
+        window.location.href = "/register";
     } else if (respondent.checked) {
         window.location.href = "/validate";
     }
 }
+
+function addressChecked() {
+    if ($(".address-id-card").is(":checked")) $(".current-address").hide();
+    else $(".current-address").show();
+}
+
+$(function () {
+    $(".nav-link").on("click", function (e) {
+        $(".nav-link").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+$(function () {
+    $(".link-pricing").on("click", function (e) {
+        $(".link-pricing").removeClass("link-orange text-decoration-underline");
+        $(this).addClass("link-orange text-decoration-underline");
+    });
+});
+
+// $(function () {
+//     $(".breadcrumb-item").on("click", function (e) {
+//         $(".breadcrumb-item").removeClass(
+//             "link-dark text-decoration-underline"
+//         );
+//         $(this).addClass("link-dark text-decoration-underline");
+//     });
+// });
