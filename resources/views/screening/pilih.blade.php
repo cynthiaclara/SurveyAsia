847e3eca368eaf942c919c6d31293597865e746a
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-10-26 06:32:56 +0700

    Update Screening

--- resources/views/screening/pilih.blade.php
+++ resources/views/screening/pilih.blade.php
@@ -3,41 +3,37 @@
 
 @section('content')
 
-  {{-- pilih --}}
-  <section class="pilih-form" id="pilih-form">
-    <div class="container">
-      <h4 class="pt-5 fw-bold text-center">
-        Memulai menggunakan Surveyasia! </h4>
-      <p class="mt-3 text-center">Pilihlah salah satu untuk kamu</p>
-      <div class="row text-center">
-        <div class="col-md-6 mt-5">
-          <input type="radio" class="btn-check" name="researcher"
-            id="researcher" autocomplete="off" value="researcher">
-          <label class="btn btn-light" for="researcher"> <img
-              src="/assets/img/akun2.png" alt="Researcher"
-              class="img-fluid rounded p-4">
-            <p class="fw-bold">Researcher</p>
-          </label>
-        </div>
-        <div class="col-md-6 mt-5">
-          <input type="radio" class="btn-check" name="options"
-            id="respondent" autocomplete="off">
-          <label class="btn btn-light" for="respondent"> <img
-              src="/assets/img/akun1.png" alt="Responden"
-              class="img-fluid rounded p-4">
-            <p class="fw-bold">Respondent</p>
-          </label>
-        </div>
+{{-- pilih --}}
+<section class="pilih-form bg-light" id="pilih-form">
+  <div class="container">
+    <h4 class="pt-5 fw-bold text-center">
+      Memulai menggunakan Surveyasia! </h4>
+    <p class="mt-3 text-center">Pilihlah salah satu untuk kamu</p>
+    <div class="row text-center">
+      <div class="col-md-6 mt-5">
+        <input type="radio" class="btn-check" name="researcher" id="researcher" autocomplete="off" value="researcher">
+        <label class="btn bg-white" for="researcher"> <img src="/assets/img/akun2.png" alt="Researcher"
+            class="img-fluid rounded p-4">
+          <p class="fw-bold">Researcher</p>
+        </label>
       </div>
-      <p class="mt-3 text-center">*Pemilihan ini hanya untuk awal pengenalan,
-        selanjutnya anda juga bisa mendapatkan
-        keduanya</p>
-      <div class="col mt-3 text-center">
-        <a class="btn btn-orange text-white w-50 text-white my-3" disabled
-          onclick="RolesButton() " role="button">Selanjutnya</a>
+      <div class="col-md-6 mt-5">
+        <input type="radio" class="btn-check" name="respondent" id="respondent" autocomplete="off" value="respondent">
+        <label class="btn bg-white" for="respondent"> <img src="/assets/img/akun1.png" alt="Responden"
+            class="img-fluid rounded p-4">
+          <p class="fw-bold">Respondent</p>
+        </label>
       </div>
     </div>
-  </section>
-  {{-- end pilih --}}
+    <p class="mt-3 text-center">*Pemilihan ini hanya untuk awal pengenalan,
+      selanjutnya anda juga bisa mendapatkan
+      keduanya</p>
+    <div class="col mt-3 text-center">
+      <a class="btn btn-orange text-white w-50 text-white my-3" disabled onclick="RolesButton() "
+        role="button">Selanjutnya</a>
+    </div>
+  </div>
+</section>
+{{-- end pilih --}}
 
 @endsection
